add7ef9e4a1683c0e6672fcaf29e65da837f3372
Syerne <shubhamyerne1994@gmail.com>
2022-05-26 20:44:30 +0530

    Added: employee daily wages calcultion with case program

--- empWagecomputation.sh
+++ empWagecomputation.sh
@@ -1,16 +1,16 @@
 echo "Welcome to Employee Wage Computation Program"
 
-isPresent=1
-randomcheck=$((RANDOM % 2))
+randomcheck=$((RANDOM % 3))
 wage_Per_Hrs=20
 
-if [ $isPresent == $randomcheck ]
-then
-	echo "employee is present"
-	working_Hrs=8
-else 
-	echo "employee is absent"
-fi
+case $randomcheck in
+	1) echo "employee is full time present"
+	   working_Hrs=8				;;
+	2) echo "employee is part time present"
+	   working_Hrs=4				;;
+	3) echo "employee is absent"
+	   working_Hrs=0				;;
+esac
 
 salary=$((wage_Per_Hrs * working_Hrs))
 echo $salary
