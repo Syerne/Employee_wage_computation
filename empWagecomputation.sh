1169f7cac54ccf7b0e27fad4c44518417898b443
Syerne <shubhamyerne1994@gmail.com>
2022-05-26 20:24:31 +0530

    Added: employee daily wages calcultion program

--- empWagecomputation.sh
+++ empWagecomputation.sh
@@ -1,11 +1,16 @@
-echo "welcome to the employee wage computation"
+echo "Welcome to Employee Wage Computation Program"
 
 isPresent=1
 randomcheck=$((RANDOM % 2))
-echo "randomcheck num:" $randomcheck
+wage_Per_Hrs=20
+
 if [ $isPresent == $randomcheck ]
 then
 	echo "employee is present"
+	working_Hrs=8
 else
 	echo "employee is absent"
 fi
+
+salary=$((wage_Per_Hrs * working_Hrs))
+echo $salary
