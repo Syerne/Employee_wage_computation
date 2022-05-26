e249f45bb0b19c6e00c579626280da53467f1b16
Syerne <shubhamyerne1994@gmail.com>
2022-05-26 20:03:49 +0530

    Added: employee is present or absent

--- empWagecomputation.sh
+++ empWagecomputation.sh
@@ -1,1 +1,11 @@
 echo "welcome to the employee wage computation"
+
+isPresent=1
+randomcheck=$((RANDOM % 2))
+echo "randomcheck num:" $randomcheck
+if [ $isPresent == $randomcheck ]
+then
+	echo "employee is present"
+else
+	echo "employee is absent"
+fi
